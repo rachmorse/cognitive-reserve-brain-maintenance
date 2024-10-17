a637f6dbf3f7cb24fbdef7c7ee5a9df0fed21963
rachmorse <rachel.morse.20@ucl.ac.uk>
2024-10-17 17:15:38 +0200

    Used styler & updated results

--- Fisher z-transformation.R
+++ Fisher z-transformation.R
@@ -1,49 +1,44 @@
+# Install and load packages
+if (!require("dplyr")) {
+  install.packages("dplyr")
+  require("dplyr")
+}
 
-if (!require("compare")) {install.packages("compare"); require("compare")}
+if (!require("compare")) {
+  install.packages("compare")
+  require("compare")
+}
 
+library(readr)
 
-waha_fc <- read_delim("/tsd/p274/data/durable/projects/p027-cr_bm/rsFC/WAHAFiltAndResid.csv", 
-                         delim = ";", escape_double = FALSE, trim_ws = TRUE)
+# Read in data that needs to be z-transformed
+waha_fc <- read_delim("/tsd/p274/data/durable/projects/p027-cr_bm/rsFC/WAHAFiltAndResid.csv",
+  delim = ";", escape_double = FALSE, trim_ws = TRUE
+)
 
+# Read in additional pre z-transformed data and post z-transformed data for another cohort
 cobra_z <- read_csv("/tsd/p274/data/durable/projects/p027-cr_bm/Umu/FC_shirer_CobraBetula/FC_Shirer90x90_Fisher-z-trans_FiltAndResid_Cobra_smallBB.csv")
 cobra_no_z <- read_csv("/tsd/p274/data/durable/projects/p027-cr_bm/Umu/FC_shirer_CobraBetula/FC_Shirer90x90_no-Fisher-z-trans_FiltAndResid_Cobra_smallBB.csv")
-
-betula_z <- read_csv("/tsd/p274/data/durable/projects/p027-cr_bm/Umu/FC_shirer_CobraBetula/FC_Shirer90x90_Fisher-z-trans_FiltAndResid_Betula_smallBB.csv")
-betula_no_z <- read_csv("/tsd/p274/data/durable/projects/p027-cr_bm/Umu/FC_shirer_CobraBetula/FC_Shirer90x90_no-Fisher-z-trans_FiltAndResid_Betula_smallBB.csv")
-
 
 # Define the Fisher Z transformation function
 fisher_z <- function(r) {
   0.5 * log((1 + r) / (1 - r))
 }
 
-# Try the  Fisher Z transformation for Cobra and compare
+# Try the  Fisher Z transformation for data that has been z-transformed to compare output of this function with already transformed data
 cobra_no_z <- cobra_no_z %>%
   mutate_at(vars(-"...1"), fisher_z)
 
-cobra_no_z <- cobra_no_z[order(row.names(cobra_no_z)),]
-cobra_z <- cobra_z[order(row.names(cobra_z)),]
+cobra_no_z <- cobra_no_z[order(row.names(cobra_no_z)), ]
+cobra_z <- cobra_z[order(row.names(cobra_z)), ]
 
+# Check that the output is the same
 identical <- compare(cobra_no_z, cobra_z)
 print(identical)
 
-# Try for Betula 
-betula_no_z <- betula_no_z %>%
-  mutate_at(vars(-"...1"), fisher_z)
-
-betula_no_z <- betula_no_z[order(row.names(betula_no_z)),]
-betula_z <- betula_z[order(row.names(betula_z)),]
-
-identical <- compare(betula_no_z, betula_z)
-print(identical)
-
-# Now transform WAHA data 
+# Now z-transform WAHA data
 waha_fc_z <- waha_fc %>%
-  mutate_at(vars(-"code",-"timepoint"), fisher_z)
+  mutate_at(vars(-"code", -"timepoint"), fisher_z)
 
-# Export waha to a CSV file
+# Export WAHA z-transformed data to a CSV file
 write.csv(waha_fc_z, "/tsd/p274/data/durable/projects/p027-cr_bm/rsFC/WAHAFiltAndResid_Fisher-z-trans.csv", row.names = FALSE)
-
-
-
-
